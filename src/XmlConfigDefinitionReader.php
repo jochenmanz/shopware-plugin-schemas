f1dff50c7ddc86ef952fbd2b669a6419d6bbf124
Benjamin Cremer <bc@shopware.com>
2016-05-10 10:25:50 +0200

    Add files

--- src/XmlConfigDefinitionReader.php
+++ src/XmlConfigDefinitionReader.php
@@ -1,1 +1,136 @@
-../../engine/Shopware/Components/Plugin/XmlConfigDefinitionReader.php
+<?php
+/**
+ * Shopware 5
+ * Copyright (c) shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+namespace Shopware\Components\Plugin;
+
+use Symfony\Component\Config\Util\XmlUtils;
+
+class XmlConfigDefinitionReader
+{
+    public function read($file)
+    {
+        try {
+            $dom = XmlUtils::loadFile($file, __DIR__.'/schema/config.xsd');
+        } catch (\Exception $e) {
+            throw $e;
+//            throw new MappingException($e->getMessage(), $e->getCode(), $e);
+        }
+
+        return $this->parseMenu($dom);
+    }
+
+    /**
+     * @param \DOMDocument $xml
+     * @return array
+     */
+    private function parseMenu(\DOMDocument $xml)
+    {
+        $xpath = new \DOMXPath($xml);
+
+        if (false === $entries = $xpath->query('//elements/element')) {
+            return;
+        }
+
+        $elements = [];
+
+        /** @var \DOMElement $entry */
+        foreach ($entries as $entry) {
+            $element = [];
+
+            $isRequired = ($entry->getAttribute('required')) ? XmlUtils::phpize($entry->getAttribute('required')) : false;
+            $type = ($entry->getAttribute('type')) ? $entry->getAttribute('type') : 'text';
+
+            $scope = ($entry->getAttribute('scope')) ? $entry->getAttribute('scope') : 'locale';
+            if ($scope === 'locale') {
+                $scope = 0;
+            } elseif ($scope === 'shop') {
+                $scope = 1;
+            } else {
+                throw new \InvalidArgumentException(sprintf("Invalid scope '%s", $scope));
+            }
+
+            $element['isRequired'] = $isRequired;
+            $element['type'] = $type;
+            $element['scope'] = $scope;
+
+            if ($position = $this->getChildren($entry, 'name')) {
+                $element['name'] = $position[0]->nodeValue;
+            }
+
+            if ($position = $this->getChildren($entry, 'store')) {
+                $element['store'] = $position[0]->nodeValue;
+            }
+
+            if ($position = $this->getChildren($entry, 'value')) {
+                $element['value'] = XmlUtils::phpize($position[0]->nodeValue);
+            } else {
+                $element['value'] = null;
+            }
+
+            foreach ($this->getChildren($entry, 'description') as $label) {
+                $lang = ($label->getAttribute('lang')) ? $label->getAttribute('lang') : 'en';
+                $element['description'][$lang] = $label->nodeValue;
+            }
+
+            foreach ($this->getChildren($entry, 'label') as $label) {
+                $lang = ($label->getAttribute('lang')) ? $label->getAttribute('lang') : 'en';
+                $element['label'][$lang] = $label->nodeValue;
+            }
+
+            $element['options'] = [];
+            foreach ($this->getChildren($entry, 'options') as $option) {
+                foreach ($option->childNodes as $node) {
+                    if (!$node instanceof \DOMElement) {
+                        continue;
+                    }
+                    $element['options'][$node->nodeName] = XmlUtils::phpize($node->textContent);
+                }
+            }
+
+            $elements[] = $element;
+        }
+
+        return $elements;
+    }
+
+    /**
+     * Get child elements by name.
+     *
+     * @param \DOMNode $node
+     * @param mixed    $name
+     *
+     * @return \DOMElement[]
+     */
+    private function getChildren(\DOMNode $node, $name)
+    {
+        $children = array();
+        foreach ($node->childNodes as $child) {
+            if ($child instanceof \DOMElement && $child->localName === $name) {
+                $children[] = $child;
+            }
+        }
+
+        return $children;
+    }
+}
--- src/XmlMenuReader.php
+++ src/XmlMenuReader.php
@@ -1,1 +1,115 @@
-../../engine/Shopware/Components/Plugin/XmlMenuReader.php
+<?php
+/**
+ * Shopware 5
+ * Copyright (c) shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+namespace Shopware\Components\Plugin;
+
+use Symfony\Component\Config\Util\XmlUtils;
+
+class XmlMenuReader
+{
+    public function read($file)
+    {
+        try {
+            $dom = XmlUtils::loadFile($file, __DIR__.'/schema/menu.xsd');
+        } catch (\Exception $e) {
+            throw $e;
+//            throw new MappingException($e->getMessage(), $e->getCode(), $e);
+        }
+
+        return $this->parseMenu($dom);
+    }
+
+    /**
+     * @param \DOMDocument $xml
+     * @return array
+     */
+    private function parseMenu(\DOMDocument $xml)
+    {
+        $xpath = new \DOMXPath($xml);
+
+        if (false === $entries = $xpath->query('//entries/entry')) {
+            return;
+        }
+
+        $menu = [];
+        $menuEntry = [];
+
+
+        foreach ($entries as $entry) {
+            if ($names = $this->getChildren($entry, 'name')) {
+                $menuEntry['name'] = $names[0]->nodeValue;
+            }
+
+            if ($controllers = $this->getChildren($entry, 'controller')) {
+                $menuEntry['controller'] = $controllers[0]->nodeValue;
+            }
+
+            if ($class = $this->getChildren($entry, 'class')) {
+                $menuEntry['class'] = $class[0]->nodeValue;
+            }
+
+            if ($active = $this->getChildren($entry, 'active')) {
+                $menuEntry['active'] = XmlUtils::phpize($active[0]->nodeValue);
+            }
+
+            if ($value = $this->getChildren($entry, 'parent')) {
+                $key = ($value[0]->getAttribute('identifiedBy')) ? $value[0]->getAttribute('identifiedBy') : 'controller';
+                $value = $value[0]->nodeValue;
+                $menuEntry['parent'] = [$key => $value];
+            }
+
+            if ($position = $this->getChildren($entry, 'position')) {
+                $menuEntry['position'] = (int) $position[0]->nodeValue;
+            }
+
+            foreach ($this->getChildren($entry, 'label') as $label) {
+                $lang = ($label->getAttribute('lang')) ? $label->getAttribute('lang') : 'en';
+                $menuEntry['label'][$lang] = $label->nodeValue;
+            }
+            $menu[] = $menuEntry;
+        }
+
+        return $menu;
+    }
+
+    /**
+     * Get child elements by name.
+     *
+     * @param \DOMNode $node
+     * @param mixed    $name
+     *
+     * @return \DOMElement[]
+     */
+    private function getChildren(\DOMNode $node, $name)
+    {
+        $children = array();
+        foreach ($node->childNodes as $child) {
+            if ($child instanceof \DOMElement && $child->localName === $name) {
+                $children[] = $child;
+            }
+        }
+
+        return $children;
+    }
+}
